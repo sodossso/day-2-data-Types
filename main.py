c8ce29a60992a42f4aa8291c99efaa7dfc21d09b
Sofia <sofianunes96@gmail.com>
2024-07-23 13:13:57 +0000

    Added information on data types with examples

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 #Data Types
-print("Hello World")
+
+#String
+#Subscripting
+print("Hello"[0])
+
+#shows that if numbers are written within quotes than the computer will read a string and concatenate both strings into one
+print("123"+"345")
+
+#Integer- whole numbers pos or negative
+
+#likewise if we remove the quotes and try to print the same function we get the result of the sum between both numbers
+print(123+345)
+
+#in order to be able to visualize better a large integer we can use underscores. These will be ignrored by the program when the code runs.
+print(123_456_789)
+
+#Float - or floating point number is a number with decimal places
+
+print(3.14159)
+
+#Bolean - can only have 2 possible types, needs to start with capital letter.
+
+True
+False
+
